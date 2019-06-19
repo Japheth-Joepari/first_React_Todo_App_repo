69fc71a987381675157eb74f5dfa7a1dc7188159
phython023 <phython023@gmail.com>
2019-06-19 12:30:34 +0100

    my First Todo App Using react

---
+++ src/Todoitems.js
@@ -0,0 +1,8 @@
+import React, { componets } from "react";
+
+const  Todoitems = () => {
+    return(
+        <h2> I render individual todo items</h2>
+    )
+}
+export  default Todoitems;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+import Todo from './Todo';
+import TodoItems from './Todoitems'
+
+ReactDOM.render(
+  <Todo />,
+  document.getElementById('root')
+);
